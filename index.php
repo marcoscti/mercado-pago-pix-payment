912459191c9748db07d34869607b707db87232b4
Marcos Cordeiro <marcosc974@gmail.com>
2025-09-30 09:01:20 -0300

    feat: implementação dos testes

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
+<?php
+require_once 'MercadoPagoPix.php';
 
-        <?php
-        require_once 'MercadoPagoPix.php';
-        header('Content-Type: application-json');
+// Corrige Content-Type
+header('Content-Type: application/json; charset=utf-8');
 
-        $dados = [
-            'value' => 1.0,
-            'email' => 'marcosc974@gmail.com',
-            'first_name' => '',
-            'last_name' => '',
-            'cpf' => ''
-        ];
-        
-        $hp = new MercadoPagoPix();
-        $hp->processPixDirect($dados);
-        //$hp->getPaymentStatusDirect(127613873172);
-        ?>
+$hp = new MercadoPagoPix();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Captura e decodifica o JSON enviado pelo fetch
+    $input = json_decode(file_get_contents('php://input'), true);
+
+    if (!$input) {
+        echo json_encode([
+            'success' => false,
+            'message' => 'Nenhum dado JSON recebido ou formato inválido'
+        ]);
+        exit;
+    }
+
+    echo $hp->processPixDirect([
+        'value' => $input['value'],
+        'email' => $input['email'],
+        'first_name' => 'Name',
+        'last_name' => 'Lastname',
+        'cpf' => '00000000000'
+    ]);
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['paymentid'])) {
+    echo $hp->getPaymentStatusDirect($_GET['paymentid']);
+} else {
+    echo json_encode([
+        'success' => false,
+        'message' => 'Argumento inválido! '
+    ]);
+}
