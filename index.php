45830351d6911e056fbb3026a7f115dd1cc37818
Marcos Cordeiro <marcosc974@gmail.com>
2025-10-01 09:41:14 -0300

    fix: origins

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require_once 'MercadoPagoPix.php';
 // Corrige Content-Type
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
-
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 $hp = new MercadoPagoPix();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
